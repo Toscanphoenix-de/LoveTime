92fff3becfcb5d2e459b6a957ae46ccf170b1e51
Toscanphoenix-de <ricco.hipp@posteo.de>
2021-03-04 20:08:19 +0100

    Added help

---
+++ app/src/main/java/com/fenni/kotlintry/HelpActivity.kt
@@ -0,0 +1,24 @@
+package com.fenni.kotlintry
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class HelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+
+        goHome()
+    }
+
+    private fun goHome() {
+
+        findViewById<FloatingActionButton>(R.id.goHome).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
