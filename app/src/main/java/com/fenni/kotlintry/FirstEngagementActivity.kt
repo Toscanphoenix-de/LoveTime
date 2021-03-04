f58559b1303d61d29f961e6dfc54a780bb5fae43
Toscanphoenix-de <ricco.hipp@posteo.de>
2021-03-04 20:04:50 +0100

    Added Engagement

---
+++ app/src/main/java/com/fenni/kotlintry/FirstEngagementActivity.kt
@@ -0,0 +1,89 @@
+package com.fenni.kotlintry
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class FirstEngagementActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+
+
+    var year = 0;
+    var month = 0
+    var day = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_engagement)
+
+        loadData()
+        pickDate()
+    }
+
+    private fun pickDate() {
+        findViewById<Button>(R.id.btn_pickEngagementDate).setOnClickListener{
+            getDateCalender()
+            DatePickerDialog(this,this,year,month,day).show()
+        }
+    }
+
+    private fun getDateCalender() {
+        val calendar = Calendar.getInstance()
+        year = calendar.get(Calendar.YEAR)
+        month = calendar.get(Calendar.MONTH)
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    }
+
+    @SuppressLint("ShowToast")
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val saveYear = year
+        val saveMonth = month +1
+
+        val sharedPref = this.getSharedPreferences("engagementDate", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        editor.putInt("year", saveYear)
+        editor.putInt("month", saveMonth)
+        editor.putInt("day", dayOfMonth)
+
+        editor.apply()
+
+        Toast.makeText(this,"Date successfully stored", Toast.LENGTH_SHORT).show()
+
+        loadData()
+
+        // Thread.sleep(2000)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun loadData(){
+        val sharedPref = this.getSharedPreferences("engagementDate", Context.MODE_PRIVATE)
+
+        if (sharedPref != null){
+            val year = sharedPref.getInt("year", 2020)
+            val month = sharedPref.getInt("month",10)
+            val date = sharedPref.getInt("day", 6)
+
+            val dateString = "$date.$month.$year"
+
+            findViewById<TextView>(R.id.testView).text = dateString
+        }else{
+            Toast.makeText(this, "Used pref settings", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/fenni/kotlintry/MainEngagedActivity.kt
@@ -0,0 +1,304 @@
+package com.fenni.kotlintry
+
+/**
+ * @author : Toscanhpoenix
+ * @version: 1.0
+ * @since: 2021-03-03*/
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.dynamic.IFragmentWrapper
+import java.time.LocalDate
+import java.time.Period
+import kotlin.math.abs
+
+class MainEngagedActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
+
+    lateinit var gestureDetector:GestureDetector
+    var x2 = 0.0f
+    var x1 = 0.0f
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+            setContentView(R.layout.activity_main_engaged)
+            val toolbar: Toolbar? = findViewById(R.id.header)
+            setSupportActionBar(toolbar)
+
+
+        dateCheckAndSet()
+        namesCheckAndSet()
+
+        gestureDetector = GestureDetector(this,this)
+
+
+    }
+
+
+    //----------------------------------------------------------------------------------------------Name
+    private fun namesCheckAndSet() {
+        val sharedPreferences = this.getSharedPreferences("names", Context.MODE_PRIVATE)
+
+        val name =
+            sharedPreferences.getString("name", "Here could be your name. Change it in settings")
+
+        findViewById<TextView>(R.id.dateBanner).text = name
+
+    }
+
+    //----------------------------------------------------------------------------------------------Option
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.help -> {
+                val intent = Intent(this, HelpActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    //----------------------------------------------------------------------------------------------Date
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun dateCheckAndSet() {
+        val sharedPref = this.getSharedPreferences("engagementDate", Context.MODE_PRIVATE)
+
+        val savedYear = sharedPref.getInt("year", 2021)
+        val savedMonth = sharedPref.getInt("month", 3)
+        val savedDay = sharedPref.getInt("day", 6)
+
+
+        findViewById<TextView>(R.id.dateBannerEngaged).text = "$savedDay.$savedMonth.$savedYear"
+
+
+        val dateToday = LocalDate.now()
+        when {
+            savedMonth < 10 -> {
+                if (savedDay < 10) {
+                    val dateString = "$savedYear-0$savedMonth-0$savedDay"
+                    onDateSetText(dateCalc(dateString, dateToday))
+                } else {
+                    val dateString = "$savedYear-0$savedMonth-$savedDay"
+                    onDateSetText(dateCalc(dateString, dateToday))
+                }
+            }
+            savedDay < 10 -> {
+                val dateString = "$savedYear-$savedMonth-0$savedDay"
+                onDateSetText(dateCalc(dateString, dateToday))
+            }
+            else -> {
+                val dateString = "$savedYear-$savedMonth-$savedDay"
+                onDateSetText(dateCalc(dateString, dateToday))
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun dateCalc(string_ourDate: String, date2: LocalDate): Period? {
+
+        println(string_ourDate)
+        val date = LocalDate.parse(string_ourDate)
+
+
+        return Period.between(date, date2)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun onDateSetText(period: Period?) {
+        if (period != null) {
+
+            val days = period.days
+            val months = period.months
+            val years = period.years
+
+
+            findViewById<TextView>(R.id.amount_days_engagement).text = days.toString()
+            findViewById<TextView>(R.id.amount_month_engagement).text = months.toString()
+            findViewById<TextView>(R.id.amount_years_engagement).text = years.toString()
+
+
+            //output
+            if (years > 0) {
+
+                if (years > 1 && months > 1)
+                    findViewById<TextView>(R.id.mainOut).text =
+                        getString(R.string.been_together_years_months, years, months)
+                else if (years >= 1 && months == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_years_month, years, months)
+                else if (years > 1 && months == 0 && days > 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_years_days, years, days)
+                else if (years > 1 && months == 0 && days == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_years_day, years, days)
+                else if (years > 1 && months == 0 && days == 0)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_years, years)
+                else if (years == 1 && months > 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_year_months, years, months)
+                else if (years == 1 && months == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_year_month, years, months)
+                else if (years == 1 && months == 0 && days > 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_year_days, years, days)
+                else if (years == 1 && months == 0 && days == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_year_day, years, days)
+                else if (years == 1 && months == 0 && days == 0)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_year, years)
+            }
+
+            if (months > 0 && years == 0) {
+                if (months > 1 && days > 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_months_days, months, days)
+                else if (years > 1 && days == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_months_day, months, days)
+                else if (months > 1 && days == 0)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_months, months)
+                else if (months == 1 && days > 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_month_days, months, days)
+                else if (months == 1 && days == 1)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_month_day, months, days)
+                else if (months == 1 && days == 0)
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_month, months)
+            }
+
+            if (days > 0 && months == 0 && years == 0) {
+                if (days > 1) {
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_days, days)
+                }
+                if (days == 1) {
+                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                        getString(R.string.been_together_day, days)
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+    //----------------------------------------------------------------------------------------------Gestures
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        gestureDetector.onTouchEvent(event)
+        when (event?.action) {
+
+            0 -> {
+                x1 = event.x
+
+            }
+            1 -> {
+                x2 = event.x
+
+
+
+                val valueX = x2 - x1
+
+
+                if (abs(valueX) > MainActivity.MIN_DISTANCE){
+                    if(x2>x1){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else{
+                        val intent = Intent(this, MainMarriedActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+                    }
+                }
+            }
+
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return false
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+
+}
+
+
