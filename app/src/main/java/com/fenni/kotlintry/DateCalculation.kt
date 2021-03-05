e86ec130b4edacbedd85ad25c5d36d05b7bfbc06
Toscanphoenix-de <ricco.hipp@posteo.de>
2021-03-05 11:38:04 +0100

    Added extra class for the calculation

--- app/src/main/java/com/fenni/kotlintry/DateCalculation.kt
+++ app/src/main/java/com/fenni/kotlintry/DateCalculation.kt
@@ -5,124 +5,126 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.*
 import androidx.annotation.RequiresApi
+import com.fenni.kotlintry.MainActivity.Companion.DAY
+import com.fenni.kotlintry.MainActivity.Companion.MONTH
+import com.fenni.kotlintry.MainActivity.Companion.YEAR
 import java.time.LocalDate
 import java.time.Period
 
-class DateCalculation : AppCompatActivity() {
+class DateCalculation(context: Context) : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun dateCheckAndSet() {
-        val sharedPref = this.getSharedPreferences("meetDate", Context.MODE_PRIVATE)
+    fun dateCheckAndSet(context: Context, string_Prefs : String, banner : Int, amount_days : Int, amount_month:Int, amount_years : Int, mainOut: Int) {
 
-        val savedYear = sharedPref.getInt("year", 0)
-        val savedMonth = sharedPref.getInt("month", 0)
-        val savedDay = sharedPref.getInt("day", 0)
+        val sharedPref = context.getSharedPreferences(string_Prefs, Context.MODE_PRIVATE)
 
-        findViewById<TextView>(R.id.dateBannerMarried).text = "$savedDay.$savedMonth.$savedYear"
+        val savedYear = sharedPref.getInt(YEAR, 0)
+        val savedMonth = sharedPref.getInt(MONTH, 0)
+        val savedDay = sharedPref.getInt(DAY, 0)
+
+
+        findViewById<TextView>(banner).text = "$savedDay.$savedMonth.$savedYear"
 
         val dateToday = LocalDate.now()
         when {
             savedMonth < 10 -> {
                 if (savedDay < 10) {
                     val dateString = "$savedYear-0$savedMonth-0$savedDay"
-                    onDateSetText(dateCalc(dateString, dateToday))
+                    onDateSetText(dateCalc(dateString, dateToday),amount_days,amount_month,amount_years,mainOut)
                 } else {
                     val dateString = "$savedYear-0$savedMonth-$savedDay"
-                    onDateSetText(dateCalc(dateString, dateToday))
+                    onDateSetText(dateCalc(dateString, dateToday),amount_days,amount_month,amount_years,mainOut)
                 }
             }
             savedDay < 10 -> {
                 val dateString = "$savedYear-$savedMonth-0$savedDay"
-                onDateSetText(dateCalc(dateString, dateToday))
+                onDateSetText(dateCalc(dateString, dateToday),amount_days,amount_month,amount_years,mainOut)
             }
             else -> {
                 val dateString = "$savedYear-$savedMonth-$savedDay"
-                onDateSetText(dateCalc(dateString, dateToday))
+                onDateSetText(dateCalc(dateString, dateToday),amount_days,amount_month,amount_years,mainOut)
             }
         }
     }
 
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun onDateSetText(period: Period?) {
+    private fun onDateSetText(period: Period?, amount_days : Int, amount_month:Int, amount_years : Int, mainOut :Int) {
         if (period != null) {
 
             val days = period.days
             val months = period.months
             val years = period.years
 
-            /*println(days)
-            println(months)
-            println(years)*/
 
-            findViewById<TextView>(R.id.amount_days_married).text = days.toString()
-            findViewById<TextView>(R.id.amount_month_married).text = months.toString()
-            findViewById<TextView>(R.id.amount_years_married).text = years.toString()
+            findViewById<TextView>(amount_days).text = days.toString()
+            findViewById<TextView>(amount_month).text = months.toString()
+            findViewById<TextView>(amount_years).text = years.toString()
 
 
             //output
             if (years > 0) {
 
                 if (years > 1 && months > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_years_months, years, months)
                 else if (years >= 1 && months == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_years_month, years, months)
                 else if (years > 1 && months == 0 && days > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_years_days, years, days)
                 else if (years > 1 && months == 0 && days == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_years_day, years, days)
                 else if (years > 1 && months == 0 && days == 0)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_years, years)
                 else if (years == 1 && months > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_year_months, years, months)
                 else if (years == 1 && months == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_year_month, years, months)
                 else if (years == 1 && months == 0 && days > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_year_days, years, days)
                 else if (years == 1 && months == 0 && days == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_year_day, years, days)
                 else if (years == 1 && months == 0 && days == 0)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_year, years)
             }
 
             if (months > 0 && years == 0) {
                 if (months > 1 && days > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_months_days, months, days)
                 else if (years > 1 && days == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_months_day, months, days)
                 else if (months > 1 && days == 0)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_months, months)
                 else if (months == 1 && days > 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_month_days, months, days)
                 else if (months == 1 && days == 1)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_month_day, months, days)
                 else if (months == 1 && days == 0)
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_month, months)
             }
 
             if (days > 0 && months == 0 && years == 0) {
                 if (days > 1) {
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_days, days)
                 }
                 if (days == 1) {
-                    findViewById<TextView>(R.id.mainOutEngagement).text =
+                    findViewById<TextView>(mainOut).text =
                         getString(R.string.been_together_day, days)
 
                 }
