2ae4d40d529ede69317d9a00af8a3a0fc681a373
Toscanphoenix-de <ricco.hipp@hotmail.de>
2021-03-11 21:55:50 +0100

    New stage

--- app/src/main/java/com/fenni/kotlintry/AboutActivity.kt
+++ app/src/main/java/com/fenni/kotlintry/AboutActivity.kt
@@ -1,14 +1,65 @@
 package com.fenni.kotlintry
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.billingclient.api.*
+import com.android.billingclient.api.BillingClient.SkuType.INAPP
+import com.fenni.kotlintry.Security.verifyPurchase
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.IOException
+import java.security.Security
 
-class AboutActivity : AppCompatActivity() {
+
+class AboutActivity : AppCompatActivity(), PurchasesUpdatedListener {
+
+    var purchaseStatus: TextView? = null
+    var purchaseButton: Button? = null
+    private var billingClient: BillingClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        purchaseStatus = findViewById<View>(R.id.purchase_status) as TextView
+        purchaseButton = findViewById<View>(R.id.purchase_button) as Button
+
+        billingClient = BillingClient.newBuilder(this)
+            .enablePendingPurchases().setListener(this).build()
+        billingClient!!.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    val queryPurchase = billingClient!!.queryPurchases(INAPP)
+                    val queryPurchases: List<Purchase>? = queryPurchase.purchasesList
+                    if (queryPurchases != null && queryPurchases.size > 0) {
+                        handlePurchases(queryPurchases)
+                    }
+                    //if purchase list is empty that means item is not purchased
+                    //Or purchase is refunded or canceled
+                    else{
+                        savePurchaseValueToPref(false);
+                    }
+                }
+            }
+
+            override fun onBillingServiceDisconnected() {}
+        })
+
+        //item Purchased
+        if (purchaseValueFromPref) {
+            purchaseButton!!.visibility = View.GONE
+            purchaseStatus!!.text = "Purchase Status : Purchased"
+        }
+        //item not Purchased
+        else {
+            purchaseButton!!.visibility = View.VISIBLE
+            purchaseStatus!!.text = "Purchase Status : Not Purchased"
+        }
 
         goHome()
     }
@@ -20,5 +71,191 @@ class AboutActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private val preferenceObject: android.content.SharedPreferences?
+        get() = applicationContext.getSharedPreferences(PREF_FILE, 0)
+
+    private val preferenceEditObject: android.content.SharedPreferences.Editor
+        get() {
+            val pref: SharedPreferences = applicationContext.getSharedPreferences(PREF_FILE, 0)
+            return pref.edit()
+
+        }
+
+    private val purchaseValueFromPref: Boolean
+        get() = preferenceObject?.getBoolean(PURCHASE_KEY, false) == true
+
+    private fun savePurchaseValueToPref(value: Boolean) {
+        preferenceEditObject.putBoolean(PURCHASE_KEY, value).commit()
+    }
+
+    //initiate purchase on button click
+    fun purchase(view: View?) {
+        //check if service is already connected
+
+        if (billingClient!!.isReady) {
+            initiatePurchase()
+        }
+        //else reconnect service
+        else {
+            billingClient = BillingClient.newBuilder(this).enablePendingPurchases().setListener(this).build()
+            billingClient!!.startConnection(object : BillingClientStateListener {
+                override fun onBillingSetupFinished(billingResult: BillingResult) {
+                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                        initiatePurchase()
+                    } else {
+                        Toast.makeText(applicationContext, "Error " + billingResult.debugMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onBillingServiceDisconnected() {}
+            })
+        }
+    }
+
+    private fun initiatePurchase() {
+
+        val skuList: MutableList<String> = ArrayList()
+        skuList.add(PRODUCT_ID)
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(INAPP)
+
+        billingClient!!.querySkuDetailsAsync(params.build())
+        { billingResult, skuDetailsList ->
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                if (skuDetailsList != null && skuDetailsList.size > 0) {
+                    val flowParams = BillingFlowParams.newBuilder()
+                        .setSkuDetails(skuDetailsList[0])
+                        .build()
+                    billingClient!!.launchBillingFlow(this@AboutActivity, flowParams)
+                } else {
+                    //try to add item/product id "purchase" inside managed product in google play console
+
+                    Toast.makeText(applicationContext, "Purchase Item not Found", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(applicationContext,
+                    " Error " + billingResult.debugMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onPurchasesUpdated(billingResult: BillingResult, purchases: List<Purchase>?) {
+        //if item newly purchased
+
+        if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
+            handlePurchases(purchases)
+        }
+        //if item already purchased then check and reflect changes
+        else if (billingResult.responseCode == BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED) {
+            val queryAlreadyPurchasesResult = billingClient!!.queryPurchases(INAPP)
+            val alreadyPurchases: List<Purchase>? = queryAlreadyPurchasesResult.purchasesList
+            if (alreadyPurchases != null) {
+                handlePurchases(alreadyPurchases)
+            }
+        }
+        //if purchase cancelled
+        else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
+            Toast.makeText(applicationContext, "Purchase Canceled", Toast.LENGTH_SHORT).show()
+        }
+        // Handle any other error msgs
+        else {
+            Toast.makeText(applicationContext, "Error " + billingResult.debugMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun handlePurchases(purchases: List<Purchase>) {
+        for (purchase in purchases) {
+            //if item is purchased
+
+            if (PRODUCT_ID == purchase.sku && purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
+                if (!verifyValidSignature(purchase.originalJson, purchase.signature)) {
+                    // Invalid purchase
+                    // show error to user
+
+                    Toast.makeText(applicationContext, "Error : Invalid Purchase", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                // else purchase is valid
+                //if item is purchased and not acknowledged
+
+
+                if (!purchase.isAcknowledged) {
+                    val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+                        .build()
+                    billingClient!!.acknowledgePurchase(acknowledgePurchaseParams, ackPurchase)
+                }
+                //else item is purchased and also acknowledged
+                else {
+                    // Grant entitlement to the user on item purchase
+                    // restart activity
+
+                    if (!purchaseValueFromPref) {
+                        savePurchaseValueToPref(true)
+                        Toast.makeText(applicationContext, "Item Purchased", Toast.LENGTH_SHORT).show()
+                        recreate()
+                    }
+                }
+            }
+            //if purchase is pending
+            else if (PRODUCT_ID == purchase.sku && purchase.purchaseState == Purchase.PurchaseState.PENDING) {
+                Toast.makeText(applicationContext,
+                    "Purchase is Pending. Please complete Transaction", Toast.LENGTH_SHORT).show()
+            }
+            //if purchase is refunded or unknown
+            else if (PRODUCT_ID == purchase.sku && purchase.purchaseState == Purchase.PurchaseState.UNSPECIFIED_STATE) {
+                savePurchaseValueToPref(false)
+                purchaseStatus!!.text = "Purchase Status : Not Purchased"
+                purchaseButton!!.visibility = View.VISIBLE
+                Toast.makeText(applicationContext, "Purchase Status Unknown", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    var ackPurchase = AcknowledgePurchaseResponseListener { billingResult ->
+        if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+            //if purchase is acknowledged
+            // Grant entitlement to the user. and restart activity
+
+            savePurchaseValueToPref(true)
+            Toast.makeText(applicationContext, "Item Purchased", Toast.LENGTH_SHORT).show()
+            recreate()
+        }
+    }
+
+    /**
+     * Verifies that the purchase was signed correctly for this developer's public key.
+     *
+     * Note: It's strongly recommended to perform such check on your backend since hackers can
+     * replace this method with "constant true" if they decompile/rebuild your app.
+     *
+     */
+    private fun verifyValidSignature(signedData: String, signature: String): Boolean {
+        return try {
+            // To get key go to Developer Console > Select your app > Development Tools > Services & APIs.
+
+            val base64Key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhDXdyUY0QCoMRhEyaM8DYqZ" +
+                    "rP/ZoBjey1Oa1dh6JZU5opFPScC+uVuOdYvX/H7iw+nQmheYkUC4Gh8A/BfpRm9fB+XdCrmYBvvTRO1dmoS7SuCGpHRENbd54lBGy" +
+                    "ft7DCEDiJoh4UTz5RvUfEb4G9Dc0ZGpLejwiBPRVol0VxUAPPoAfA6eaG1gWwr4uPpGqOjhwvhmpWYkw8/Ku6IJu7+DOgPZqpgC9vV/990FVj2CUc/" +
+                    "9EdMel3rcIvd9GuFzRN2oeQxbw8br0EizLmsOcCE6z5xM6k27YbI8fvog06k8gDw1FoMwyg3IlDHlItQaIfGNSDPYNhJUJKdreUEjF/wIDAQAB"
+            verifyPurchase(base64Key,signedData,signature)
+        } catch (e: IOException) {
+            false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (billingClient != null) {
+            billingClient!!.endConnection()
+        }
+    }
+
+    companion object {
+        const val PREF_FILE = "Donation"
+        const val PURCHASE_KEY = "donation_lovetime"
+        const val PRODUCT_ID = "donation_lovetime"
     }
 }
